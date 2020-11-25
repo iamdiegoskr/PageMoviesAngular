2fd8673aeba32f6391e8205c27eed6f646ed1be8
diego martinez <juandiego337@hotmail.com>
2020-11-25 00:49:00 -0500

    initial structure of the project, structure of header is ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+  
   ],
   imports: [
     BrowserModule
